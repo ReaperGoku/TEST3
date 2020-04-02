b65d3641463a36a1e2f16da01e081024f8254c6a
ReaperGoku <58998275+ReaperGoku@users.noreply.github.com>
2020-04-02 08:05:07 +0530

    change 02/04/2020 8.05AM

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,4 +1,4 @@
 module.exports = async (client) => {
     console.log('I am online');
-    client.user.setActivity(': DM me for any issue!', { type: 'WATCHING' });
+    client.user.setActivity(' This server', { type: 'WATCHING' });
 }
